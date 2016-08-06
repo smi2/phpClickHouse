19e7980d1282176ec7ad0a770bde5b3386c98515
Igor <isublimity@gmail.com>
2016-08-06 17:04:31 +0300

    Exception

--- example/exam11_errors.php
+++ example/exam11_errors.php
@@ -2,32 +2,32 @@
 include_once __DIR__.'/../include.php';
 
 
-//
-//$config=['host'=>'x','port'=>'8123','username'=>'x','password'=>'x'];
-//$db=new ClickHouseDB\Client($config);
-//
-//try
-//{
-//    $db->ping();
-//}
-//catch (ClickHouseDB\QueryException $E)
-//{
-//    echo "ERROR:".$E->getMessage()."\nOK\n";
-//}
-//
-//
-//$config=['host'=>'192.168.1.20','port'=>'8123','username'=>'x','password'=>'x'];
-//$db=new ClickHouseDB\Client($config);
-//try
-//{
-//    $db->ping();
-//}
-//catch (ClickHouseDB\QueryException $E)
-//{
-//    echo "ERROR:".$E->getMessage()."\nOK\n";
-//}
-//
-//
+
+$config=['host'=>'x','port'=>'8123','username'=>'x','password'=>'x'];
+$db=new ClickHouseDB\Client($config);
+
+try
+{
+    $db->ping();
+}
+catch (ClickHouseDB\QueryException $E)
+{
+    echo "ERROR:".$E->getMessage()."\nOK\n";
+}
+
+
+$config=['host'=>'192.168.1.20','port'=>'8123','username'=>'x','password'=>'x'];
+$db=new ClickHouseDB\Client($config);
+try
+{
+    $db->ping();
+}
+catch (ClickHouseDB\QueryException $E)
+{
+    echo "ERROR:".$E->getMessage()."\nOK\n";
+}
+
+
 $config=['host'=>'192.168.1.20','port'=>'8123','username'=>'default','password'=>''];
 $db=new ClickHouseDB\Client($config);
 try
