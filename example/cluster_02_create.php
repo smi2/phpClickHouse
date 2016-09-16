46ad3f9a32f38c4fa94826efedeac243a8f1b945
Igor <isublimity@gmail.com>
2016-09-16 12:51:28 +0300

    proto

--- example/cluster_02_create.php
+++ example/cluster_02_create.php
@@ -5,28 +5,7 @@ include_once __DIR__ . '/lib_example.php';
 // load production config
 $config = include_once __DIR__ . '/../../_clickhouse_config_product.php';
 
-$cl = new ClickHouseDB\Cluster($config);
-//$sql_up=['CREATE DATABASE IF NOT EXISTS cluster_tests',
-////
-//'
-//CREATE TABLE IF NOT EXISTS cluster_tests.sum_views_sharded (
-//    event_date Date DEFAULT toDate(event_time),
-//    event_time DateTime DEFAULT now(),
-//    site_id Int32,
-//    views Int32,
-//    bad_views Int32
-//) ENGINE = ReplicatedSummingMergeTree(\'/clickhouse/tables/{aggr_replica}/aggr/sum_views_sharded\', \'{replica}\', event_date, (event_date,event_time,site_id), 8192)
-//',
-//'
-//CREATE TABLE IF NOT EXISTS cluster_tests.sum_views AS cluster_tests.sum_views_sharded ENGINE = Distributed(aggr, cluster_tests, sum_views_sharded, rand())
-//'
-//];
-//$sql_down=[
-//        'DROP DATABASE IF EXISTS cluster_tests',
-//        'DROP TABLE IF EXISTS cluster_tests.sum_views_sharded',
-//        'DROP TABLE IF EXISTS cluster_tests.sum_views',
-//];
-// ----------------------------------------------------------------------
+
 $cl = new ClickHouseDB\Cluster($config);
 
 $cl->setScanTimeOut(2.5); // 2500 ms
