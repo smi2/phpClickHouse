e9e71a91997901ab6395eb4aea15cd5f75970b52
igor <isublimity@gmail.com>
2017-09-01 12:34:39 +0300

    Conditions changes

--- src/Query/Degenerations/Conditions.php
+++ src/Query/Degenerations/Conditions.php
@@ -24,6 +24,47 @@ class Conditions implements \ClickHouseDB\Query\Degeneration
         }
     }
 
+
+    static function __ifsets($matches,$markers,$else=false)
+    {
+        $content_false='';
+
+        if ($else)
+        {
+            list($condition, $preset ,$variable, $content_true, $content_false) = $matches;
+        }
+        else
+        {
+            list($condition, $preset ,$variable, $content_true) = $matches;
+        }
+        $preset=strtolower($preset);
+
+        if ($preset=='set')
+        {
+            return (isset($markers[$variable]) && !empty($markers[$variable]))   ? $content_true: $content_false;
+        }
+        if ($preset=='bool')
+        {
+            return (isset($markers[$variable]) && is_bool($markers[$variable]) && $markers[$variable]==true)
+                ? $content_true
+                : $content_false;
+        }
+        if ($preset=='string')
+        {
+            return (isset($markers[$variable]) && is_string($markers[$variable]) && strlen($markers[$variable]))
+                ? $content_true
+                : $content_false;
+        }
+        if ($preset=='int')
+        {
+            return (isset($markers[$variable]) && intval($markers[$variable])<>0 )
+                ? $content_true
+                : $content_false;
+        }
+
+        return '';
+    }
+
     /**
      * @param $sql
      * @return mixed
@@ -31,6 +72,7 @@ class Conditions implements \ClickHouseDB\Query\Degeneration
     public function process($sql)
     {
         $markers=$this->bindings;
+
         // 2. process if/else conditions
         $sql = preg_replace_callback('#\{if\s(.+?)}(.+?)\{else}(.+?)\{/if}#sui', function ($matches) use ($markers) {
             list($condition, $variable, $content_true, $content_false) = $matches;
@@ -39,26 +81,6 @@ class Conditions implements \ClickHouseDB\Query\Degeneration
                 ? $content_true
                 : $content_false;
         }, $sql);
-
-
-        // 2. process if/else conditions
-        $sql = preg_replace_callback('#\{ifint\s(.+?)}(.+?)\{else}(.+?)\{/if}#sui', function ($matches) use ($markers) {
-            list($condition, $variable, $content_true, $content_false) = $matches;
-
-            return (isset($markers[$variable]) && intval($markers[$variable])<>0 )
-                ? $content_true
-                : $content_false;
-        }, $sql);
-
-
-        $sql = preg_replace_callback('#\{ifint\s(.+?)}(.+?)\{/if}#sui', function ($matches) use ($markers) {
-            list($condition, $variable, $content) = $matches;
-
-            if (isset($markers[$variable]) && intval($markers[$variable])<>0 ) {
-                return $content;
-            }
-        }, $sql);
-
 
         // 3. process if conditions
         $sql = preg_replace_callback('#\{if\s(.+?)}(.+?)\{/if}#sui', function ($matches) use ($markers) {
@@ -69,9 +91,9 @@ class Conditions implements \ClickHouseDB\Query\Degeneration
             }
         }, $sql);
 
-
-
-
+        // 1. process if[set|int]/else conditions
+        $sql = preg_replace_callback('#\{if(.{1,}?)\s(.+?)}(.+?)\{else}(.+?)\{/if}#sui',  function ($matches) use ($markers) {return  self::__ifsets($matches,$markers,true);  }, $sql);
+        $sql = preg_replace_callback('#\{if(.{1,}?)\s(.+?)}(.+?)\{/if}#sui',  function ($matches) use ($markers) { return self::__ifsets($matches,$markers,false);  }, $sql);
 
         return $sql;
     }
