84d9c2df5774233cfb99789f9ffd98af6b560387
Simon Podlipsky <simon@podlipsky.net>
2018-07-08 22:45:40 +0300

    ClickHouse Exception

--- src/Exception/DatabaseException.php
+++ src/Exception/DatabaseException.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ClickHouseDB\Exception;
 
-class DatabaseException extends QueryException {}
+final class DatabaseException extends QueryException implements ClickHouseException
+{
+}
