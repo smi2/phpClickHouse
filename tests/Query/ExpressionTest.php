7c14c6044d34ed22decf26b16f5d00a0a3befd07
Anton Kolenkov <dorantor@gmail.com>
2018-10-19 02:08:59 +0300

    Code formatting - aligned = signs as expected by phpcs.

--- tests/Query/ExpressionTest.php
+++ tests/Query/ExpressionTest.php
@@ -13,18 +13,18 @@ final class ExpressionTest extends TestCase
     public function testToString() : void
     {
         $expressionString = "UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')";
-        $expr = new Expression($expressionString);
+        $expressionObject = new Expression($expressionString);
 
         self::assertEquals(
             $expressionString,
-            (string) $expr
+            (string) $expressionObject
         );
     }
 
     public function testExpressionValueForInsert() : void
     {
         $expressionString = "UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')";
-        $preparedValue = FormatLine::Insert([new Expression($expressionString)]);
+        $preparedValue    = FormatLine::Insert([new Expression($expressionString)]);
 
         self::assertEquals(
             $expressionString,
