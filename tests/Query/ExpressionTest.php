c7944e4d7b2fb6655daa2ae967288f8b5b895e29
Šimon Podlipský <simon@podlipsky.net>
2018-10-18 21:50:31 +0300

    Update tests/Query/ExpressionTest.php

--- tests/Query/ExpressionTest.php
+++ tests/Query/ExpressionTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExpressionTest extends TestCase
 {
-    public function testToString()
+    public function testToString() : void
     {
         $expressionString = "UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')";
         $expr = new Expression($expressionString);
