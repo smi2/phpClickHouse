9d22259c53e3fe43ceb8bfda54e48d77bd8f5928
Anton Kolenkov <dorantor@gmail.com>
2018-10-18 15:10:59 +0300

    Updated docblocks and added typehint.

--- src/Query/Expression.php
+++ src/Query/Expression.php
@@ -2,6 +2,10 @@
 
 namespace ClickHouseDB\Query;
 
+/**
+ * Pass expression "as is" to be sent and executed at server.
+ * P.ex.: `new Expression("UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')");`
+ */
 class Expression
 {
     /**
@@ -9,22 +13,12 @@ class Expression
      */
     private $expression;
 
-    /**
-     * Expression constructor.
-     * Pass expression "as is" it should be sent and executed at server.
-     * P.ex.: `new Expression("UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')");`
-     *
-     * @param string $expression
-     */
     public function __construct(string $expression)
     {
         $this->expression = $expression;
     }
 
-    /**
-     * @return string
-     */
-    public function __toString()
+    public function __toString(): string
     {
         return $this->expression;
     }
