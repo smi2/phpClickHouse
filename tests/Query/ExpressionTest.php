c08cf6a605f8a79b4cdeaae60df9cc63ae2ce6ae
Anton Kolenkov <dorantor@gmail.com>
2018-10-18 20:07:40 +0300

    Added ExpressionTest.

---
+++ tests/Query/ExpressionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ClickHouseDB\Tests\Query;
+
+use ClickHouseDB\Query\Expression;
+use ClickHouseDB\Quote\FormatLine;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testToString()
+    {
+        $expressionString = "UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')";
+
+        $expr = new Expression($expressionString);
+
+        $this->assertEquals(
+            $expressionString,
+            (string) $expr
+        );
+    }
+
+    public function testExpressionValueForInsert()
+    {
+        $expressionString = "UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')";
+
+        $preparedValue = FormatLine::Insert([new Expression($expressionString)]);
+        
+        $this->assertEquals(
+            $expressionString,
+            $preparedValue
+        );
+    }
+}
