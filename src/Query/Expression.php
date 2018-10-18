57d01c529b63b84b329fd386f6c802ea4a94315f
Šimon Podlipský <simon@podlipsky.net>
2018-10-18 15:07:31 +0300

    Update src/Query/Expression.php

--- src/Query/Expression.php
+++ src/Query/Expression.php
@@ -11,7 +11,7 @@ class Expression
 
     /**
      * Expression constructor.
-     * Pass expression "as is" it should be sent and executed at server.
+span class="pl-s1">      * Pass expression "as is" to be sent and executed at server.
      * P.ex.: `new Expression("UUIDStringToNum('0f372656-6a5b-4727-a4c4-f6357775d926')");`
      *
      * @param string $expression
